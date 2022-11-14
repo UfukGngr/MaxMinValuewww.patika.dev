81a91dbb3688d0383078cd8ce3a26442a3dc5b0a
UfukGngr <ufukgngr94@gmail.com>
2022-11-14 15:28:21 +0300

    www.patika.dev

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int n,num,maxValue=0,temp,minValue=0,temp2;
+        int n,num,maxValue=0,temp,minValue=0;
 
 
         Scanner scan=new Scanner(System.in);
@@ -13,18 +12,14 @@ public class Main {
         for (int i=1;i<=n;i++){
             System.out.print(i+". sayıyı giriniz: ");
             num=scan.nextInt();
+            //6,8,4,12
+            temp=maxValue;
+            maxValue=num;
 
-            temp=maxValue;//temp=0 max=0
-            temp2=minValue;
-            maxValue=num;//max=6 num=6
-            minValue=num;
             if(temp>num){
                 maxValue=temp;
+                minValue=num;
             }
-            if(temp2<num){
-                minValue=temp;
-            }
-
         }
         System.out.println("En büyük sayı: "+ maxValue);
         System.out.println("En küçük sayı: "+ minValue);
